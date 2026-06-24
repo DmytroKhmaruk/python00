dbe4321851ac4c77682cf1216b8e97abd94863ff
Dmytro Khmaruk <dkhmaruk@c2r4p4.42vienna.com>
2026-06-24 12:01:30 +0200

    redu ex7, now it is correct

--- ex7/ft_seed_inventory.py
+++ ex7/ft_seed_inventory.py
@@ -1,9 +1,11 @@
 def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
-    if seed_type == "tomato":
-        print("Tomato seeds:", quantity, unit, "available")
-    elif seed_type == "carrot":
-        print("Carrot seeds:", quantity, unit, "total")
-    elif seed_type == "lettuce":
-        print("Lettuce seeds:", "covers", quantity, "square meters")
+    if unit == "packets":
+        print(f"{seed_type.capitalize()} seeds: {quantity} {unit} available")
+    elif unit == "grams":
+        print(f"{seed_type.capitalize()} seeds: {quantity} {unit} total")
+    elif unit == "area":
+        print(
+            f"{seed_type.capitalize()} seeds: covers {quantity} square meters"
+            )
     else:
-        print("Unknown unit")
+        print("Unknown unit type")
